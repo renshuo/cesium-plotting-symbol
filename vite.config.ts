a12698bbf0987f891723a9f78603bb5844284067
renshuo <renshuoo@qq.com>
2022-06-10 12:03:44 +0800

    更新npm, vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: "src/cps/index.js",
+      name: 'cesium-plotting-symbol',
+    },
+    rollupOptions: {
+      external: ['vue', 'cesium']
+    }
+  }
 })

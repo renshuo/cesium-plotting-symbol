4c17c6dd8600f5ec566a786d8988d6f9957aed67
renshuo <renshuoo@qq.com>
2022-08-01 15:21:50 +0800

    更新station的缩放参数最大值，以及初始值

--- src/cps/Model/Station.ts
+++ src/cps/Model/Station.ts
@@ -5,13 +5,13 @@ export default class Station extends Model {
   constructor (p, viewer, layer) {
     super({
       type: '地面站',
-      uri: 'station.gltf',
-      scale: 2000,
+      uri: 'station.glb',
+      scale: 500,
       ...p
     }, viewer, layer)
     this.propDefs.push(
       { name: 'uri', title: '图标', type: 'string', editable: false },
-      { name: 'scale', title: '缩放', type: 'number', editable: true, min: 1, max: 100, step: 1 },
+      { name: 'scale', title: '缩放', type: 'number', editable: true, min: 1, max: 1000, step: 1 },
     )
   }
 }
